16374a5608746daaff0aefaddcdd3465c80ae3db
flowayne63 <tiktokpsychiater@gmail.com>
2026-04-11 01:53:57 +0200

    added proper User model to models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -14,3 +16,12 @@ class PublicProfile(db.Model):
 
     def __repr__(self):
         return f'<PublicProfile {self.username} on {self.platform} ({self.category})>'
+
+
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
