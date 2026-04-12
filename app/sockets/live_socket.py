1e3507887b501e40d9ef590e6d7f7a37efce46ab
flowayne63 <tiktokpsychiater@gmail.com>
2026-04-12 08:19:38 +0200

    Socket-Events: join_stream, leave_stream, connect, disconnect für Stream-Rooms implementiert (Namespace /live)

--- app/sockets/live_socket.py
+++ app/sockets/live_socket.py
@@ -1,5 +1,5 @@
-from flask import session
-from flask_socketio import join_room, leave_room, emit
+from flask import session, request
+from flask_socketio import join_room, leave_room, emit, Namespace
 from collections import defaultdict
 
 # In-Memory Viewer-Count pro Stream (MVP, später Redis)
@@ -29,3 +29,29 @@ def handle_leave_stream(socketio, sid, stream_id):
     leave_room(room, sid=sid)
     viewer_counts[stream_id] = max(0, viewer_counts[stream_id] - 1)
     socketio.emit("viewer_update", {"stream_id": stream_id, "viewer_count": viewer_counts[stream_id]}, room=room)
+
+class LiveNamespace(Namespace):
+    def on_connect(self):
+        pass
+
+    def on_disconnect(self):
+        pass
+
+    def on_join_stream(self, data):
+        stream_id = data.get("stream_id")
+        sid = request.sid
+        if stream_id:
+            handle_join_stream(self.server, sid, stream_id)
+
+    def on_leave_stream(self, data):
+        stream_id = data.get("stream_id")
+        sid = request.sid
+        if stream_id:
+            handle_leave_stream(self.server, sid, stream_id)
+
+    # Platzhalter für weitere Events (send_message, send_like, send_gift)
+
+
+def init_app(app):
+    socketio = app.socketio
+    socketio.on_namespace(LiveNamespace("/live"))
